Now have outputs and gradient descent, but the maths is definitely wrong...

Guesser.py
@@ -2,32 +2,44 @@ from Neuron import Neuron
 from Layer import Layer
 from Network import Network
 import numpy as np
+from matplotlib import pyplot
 
-input_neuron = [Neuron(0)]
+iterations = 40000
+
+# Defines input neuron
+input_neuron = [Neuron(180)]
 input_layer = Layer(1, input_neuron)
 
+# Defines hidden layer
 hidden_neurons = []
 for i in range(128):
-    hidden_neurons.append(Neuron(np.random.ranf()))
+    hidden_neurons.append(Neuron(round(np.random.ranf(), 2)))
 hidden_layer = Layer(128, hidden_neurons)
 
-output_neuron = [Neuron(0)]
+# Defines output neuron
+output_neuron = [Neuron(round(np.random.ranf(), 2))]
 output_layer = Layer(1, output_neuron)
 
+# Randomly sets up weights
 layers = [input_layer, hidden_layer, output_layer]
 weightlists = [[], []]
 for weightlist in weightlists:
     for i in range(128):
-        weightlist.append(np.random.ranf())
+        weightlist.append(round(np.random.ranf(), 2))
 
+# Initializes network
 network = Network(layers, weightlists)
 
-for layer in network.layers:
-    print("New Layer: ")
-    for neuron in layer.neurons:
-        print("Neuron with value = {}".format(neuron.value))
+# Trains the network
+costs = []
+for i in range(iterations):
+    x = round(10 * np.random.ranf(), 2)
+    y = round(np.sin(x), 2)
+    network.layers[0].neurons[0].value = x
+    for j in range(len(network.layers) - 1):
+        network.layers[j + 1].backfeed(network.layers[j], network.weightlists[j])
 
-for weightlist in network.weightlists:
-    print("weightlist: ")
-    for weight in weightlist:
-        print("Weight: {}".format(weight))
+    output_value = network.layers[-1].neurons[0].value
+    costs.append(network.get_cost(output_value, y))
+    network.gradient_descent(costs, 0.1)
+    print("Iteration: {}, Cost: {}, Output: {}, Expected Output: {}".format(str(i), str(costs[i]), str(output_value), str(y)))

Network.py
@@ -1,6 +1,7 @@
 """
 Defines network
 """
+import numpy as np
 
 class Network(object):
     """
@@ -12,7 +13,7 @@ class Network(object):
 
     def __init__(self, layers, weightlists):
         self.layers = layers
-        self.weightlist = weightlists
+        self.weightlists = weightlists
 
     @property
     def weightlists(self):
@@ -65,3 +66,22 @@ class Network(object):
             layers(List): List of layer objects
         """
         self._layers = layers
+
+
+    def get_cost(self, output, training_value):
+        """
+        Finds the cost of the network
+
+        Args:
+            output(List): A list of correct values from training data
+        Returns:
+            cost(float): The average cost of network
+        """
+        cost = (output - training_value)
+
+        return round(cost, 2)
+
+
+    def gradient_descent(self, costs, learning_rate=1):
+        for i in range(len(self.weightlists)):
+            self.weightlists[i] -= learning_rate * np.gradient(self.weightlists[i])